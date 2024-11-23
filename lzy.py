0e3d0f7e9fd0c808da80a5dd54bfafc71b7ead2a
Long Wanbin <999999+npu-0@users.noreply.github.com>
2024-11-23 15:40:33 +0800

    change code

--- lzy.py
+++ lzy.py
@@ -9,3 +9,9 @@ x2 = torch.rand((3, 8)).to(dev)
 
 y1 = torch.einsum('bs,st->bt', x1, x2)
 print(torch_xla._XLAC._get_xla_tensors_text([y1]))
+
+y1 = y1 + x2
+print(torch_xla._XLAC._get_xla_tensors_text([y1]))
+
+xm.mark_step()
+print(torch_xla._XLAC._get_xla_tensors_text([y1]))
