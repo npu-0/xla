bbf84287d1793ccf3fc77982a33d42f3ba44d2ec
Long Wanbin <999999+npu-0@users.noreply.github.com>
2024-11-23 15:56:42 +0800

    change code

--- env.sh
+++ env.sh
@@ -3,6 +3,7 @@ export XLA_IR_DEBUG=1
 export XLA_HLO_DEBUG=1
 export XLA_FLAGS="--xla_dump_to=/tmp/dir_name"
 export TF_CPP_MIN_LOG_LEVEL=0
-export TF_CPP_MAX_LOG_LEVEL=100
+export TF_CPP_MAX_VLOG_LEVEL=100
 export XLA_SAVE_HLO_FILE="/tmp/dir_name/HLO"
 export XLA_DUMP_HLO_GRAPH=1
+export PT_XLA_DEBUG_LEVEL=4
--- lzy.py
+++ lzy.py
@@ -15,3 +15,31 @@ print(torch_xla._XLAC._get_xla_tensors_text([y1]))
 
 xm.mark_step()
 print(torch_xla._XLAC._get_xla_tensors_text([y1]))
+
+def dummy_step(x, y, loss, acc=False):
+  z = torch.einsum('bs,st->bt', y, x)
+  step_loss = z.sum().view(1,)
+  if acc:
+    loss = torch.cat((loss, step_loss))
+  else:
+    loss = step_loss
+  xm.mark_step()
+  return loss
+
+
+import time
+def measure_time(acc=False):
+  exec_times = []
+  iter_count = 1
+  x = torch.rand((512, 8)).to(dev)
+  y = torch.rand((512, 512)).to(dev)
+  loss = torch.zeros(1).to(dev)
+  for i in range(iter_count):
+    tic = time.time()
+    loss = dummy_step(x, y, loss, acc=acc)
+    toc = time.time()
+    exec_times.append(toc - tic)
+  return exec_times
+
+dyn = measure_time(acc=True) # acc= True Results in dynamic graph
+st = measure_time(acc=False) # Static graph, computation shape, inputs and output shapes don't change
